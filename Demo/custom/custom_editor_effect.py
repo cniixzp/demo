f95a3cf8a28ea2adbe1db8fa70dd2d61e77fced1
cniixzp <547775099@qq.com>
2023-12-20 19:58:38 +0800

    从这个版本开始

--- Demo/custom/custom_editor_effect.py
+++ Demo/custom/custom_editor_effect.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {134257276: {'model_id': 134247958, 'preview_icon': '', 'name': u'\u91d1\u9f99\u73af\u7ed5buff', 'describe': 134257276, 'key': 134257276, 'res_identify': '', 'scale': 1.0, 'uid': '134257276', 'source_effect': 104139}, 134228025: {'model_id': 134218637, 'preview_icon': '', 'name': u'\u9f99\u5e99\u9ed1\u96fe', 'describe': 134228025, 'key': 134228025, 'res_identify': '', 'scale': 1.0, 'uid': '134228025', 'source_effect': 101891}}
+data = {134257276: {'model_id': 134247958, 'preview_icon': '', 'name': u'\u91d1\u9f99\u73af\u7ed5buff', 'describe': 134257276, 'key': 134257276, 'res_identify': '', 'scale': 1.0, 'uid': '134257276', 'source_effect': 104139}, 134228025: {'model_id': 134218637, 'preview_icon': '', 'name': u'\u9f99\u5e99\u9ed1\u96fe', 'describe': 134228025, 'key': 134228025, 'res_identify': '', 'scale': 1.0, 'uid': '134228025', 'source_effect': 101891}, 134248232: {'model_id': 134245438, 'preview_icon': '', 'name': u'\u706b\u7130\u5200\u5149', 'describe': 134248232, 'key': 134248232, 'res_identify': '', 'scale': 1.0, 'uid': '134248232', 'source_effect': 101595}}
