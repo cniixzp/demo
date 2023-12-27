2b03c9bdac1522215e789f384ad1fc9bb36f83ce
cniixzp <547775099@qq.com>
2023-12-27 16:03:39 +0800

    合并了两个地图的版本

--- Demo/Demo/custom/custom_editor_effect.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding:utf-8 -*-
-_reload_all = True
-
-
-data = {134257276: {'model_id': 134247958, 'preview_icon': '', 'name': u'\u91d1\u9f99\u73af\u7ed5buff', 'describe': 134257276, 'key': 134257276, 'res_identify': '', 'scale': 1.0, 'uid': '134257276', 'source_effect': 104139}, 134228025: {'model_id': 134218637, 'preview_icon': '', 'name': u'\u9f99\u5e99\u9ed1\u96fe', 'describe': 134228025, 'key': 134228025, 'res_identify': '', 'scale': 1.0, 'uid': '134228025', 'source_effect': 101891}, 134248232: {'model_id': 134245438, 'preview_icon': '', 'name': u'\u706b\u7130\u5200\u5149', 'describe': 134248232, 'key': 134248232, 'res_identify': '', 'scale': 1.0, 'uid': '134248232', 'source_effect': 101595}, 134249817: {'model_id': 134241751, 'preview_icon': '', 'name': u'\u767d\u8272\u5149\u6548\u5f39\u9053\u62d6\u5c3e', 'describe': 134249817, 'key': 134249817, 'res_identify': '', 'scale': 1.0, 'uid': '134249817', 'source_effect': 104857}}
